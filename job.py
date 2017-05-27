d8547a9be78bda1d67eb22b27d6baeae8ad9476e
U-BITTWARE\Jforkey <Jforkey@eng-jf-laptop.corp.bittware.com>
2017-05-27 13:03:00 -0400

    This version doesn't work because you can only await a coroutine once.

--- job.py
+++ job.py
@@ -2,36 +2,6 @@
 import asyncio
 import sys
 
-class JobProtocol(asyncio.SubprocessProtocol):
-    def __init__(self, j):
-        self.j = j
-        self.done_future = asyncio.Future(loop=j.loop)
-
-    def pipe_data_received(self, fd, data):
-        if fd == 1:
-            self.j.default_stdout_handler(data)
-        if fd == 2:
-            self.j.default_stderr_handler(data)
-        if fd < 0 or fd > 2:
-            raise ValueError('Unknown fd number "{}"'.format(fd))
-
-    def process_exited(self):
-        self.done_future.set_result(True)
-
-    @asyncio.coroutine
-    def done(self):
-        # ??? Should I be telling the event loop about this future.
-        #asyncio.ensure_future(self.done_future)
-        yield from self.done_future
-
-def on_pid(future):
-    pid = future.result()
-    print('pid={}'.format(pid))
-
-def on_date(future):
-    date = future.result()
-    print('date="{}"'.format(date.decode('ascii').rstrip()))
-
 # job.start()
 # job.add_start_callback()
 # job.cancel()
@@ -40,33 +10,42 @@ def on_date(future):
 # job.pause() ??
 #
 class job:
-    def __init__(self, loop, save_stdout=False, save_stderr=False):
+    def __init__(self, loop, save_stdout=False, save_stderr=False, limit=(1024*4)):
         self.loop = loop
-        self.stdout = bytearray()
-        self.stderr = bytearray()
+        #self.stdout = bytearray()
+        #self.stderr = bytearray()
         self.save_stdout = save_stdout
         self.save_stderr = save_stderr
-        self.stdout_future = asyncio.Future(loop=loop)
-        self.stderr_future = asyncio.Future(loop=loop)
-        self.start_future = asyncio.Future(loop=loop)
-        self.finish_future = asyncio.Future(loop=loop)
+        self.limit = limit
 
     def command(self, *args):
         self.command = args
 
-    def __await__(self):
+    async def run(self):
         create = asyncio.create_subprocess_exec(
-                                           *self.command)
-        asyncio.ensure_future(self.finish_future)
-        self.process = yield from create
-        pid=self.process.pid
-        print('Process started, pid="{}"'.format(pid))
-        yield from self.process.wait()
-        print('process finished, pid="{}"'.format(pid))
+            *self.command,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE)
+        self.process = await create
+
+    async def finish(self):
+        await self.process.wait()
 
     async def print_stdout(self):
         async for data in self.process.read():
             print(data)
+
+    async def stdout(self):
+        data = await self.process.stdout.read(self.limit)
+        return data
+
+    async def stderr(self):
+        data = await self.process.stderr.read(self.limit)
+        return data
+
+    async def output(self):
+        data = await self.process.read()
+        return data
 
     def default_output_handler(self, data, save, buf):
         print(data.decode('ascii'))
@@ -79,20 +58,72 @@ class job:
     def default_stderr_handler(self, data):
         self.default_output_handler(b'stderr: ' + data, self.save_stderr, self.stderr)
 
+class chunk:
+    def __init__(self, j):
+        self.j = j
+
+    def __aiter__(self):
+        return self
+
+    @asyncio.coroutine
+    def __anext__(self):
+        val = yield from self.j.process.stdout.read(1000)
+        if val == b'':
+            raise StopAsyncIteration
+        return val
+
+#@asyncio.coroutine
+#def chunk(j):
+#    yield from j.process.read()
+
+#async def printer(j):
+#    ch = chunk(j)
+#    async for data in ch:
+#        print('*** stdout ***')
+#        print(data.decode().rstrip())
+#    return 'fin'
+
+async def printer(io, name):
+    while True:
+        data = await io
+        if not data:
+            break
+        print('*** {} ***'.format(name))
+        print(data.decode().rstrip())
+
+#async def printer(j,fd):
+#    if fd == 1:
+#         data = await j.stdout()
+#         print('*** stdout ***')
+#         print(data)
+#    if fd == 2:
+#        async for data in j.stderr():
+#            print('*** stderr ***')
+#            print(data)
+#    if fd == 3:
+#        async for data in j.output():
+#            print('*** either ***')
+#            print(data)
+    
+async def main(loop):
+    j = job(loop, save_stdout=True, save_stderr=True)
+    j.command('./test2.sh', '5')
+    await j.run()
+    print('job started: {}'.format(j.process.pid))
+    #asyncio.ensure_future(printer(j))
+    loop.create_task(printer(j.stdout(), 'STDOUT'))
+    loop.create_task(printer(j.stderr(), 'STDERR'))
+    await j.finish()
+    print('job finished: {}'.format(j.process.pid))
+
 if sys.platform == "win32":
     loop = asyncio.ProactorEventLoop()
     asyncio.set_event_loop(loop)
 else:
     loop = asyncio.get_event_loop()
 
-j = job(loop, save_stdout=True, save_stderr=True)
-j.command('./test2.sh', '5')
-
-async def main(j):
-    await j
-    await j
-    
-results = loop.run_until_complete(main(j))
+#f = asyncio.Future(loop=loop)
+results = loop.run_until_complete(main(loop))
 loop.close()
 
 # j.cmd = ['sleep', '1']
