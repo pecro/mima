820a4a5bc0a30e7b6834fd40d70fd5e78a6aac5c
Jason Forkey <jforkey@bittware.com>
2017-05-30 18:12:53 -0400

    added a helper class to allow async iteration over process output

--- job.py
+++ job.py
@@ -9,11 +9,27 @@ import sys
 # job.
 # job.pause() ??
 #
+'''
+A helper class to allow async iteration on process output
+'''
+class output:
+    def __init__(self, cor, limit):
+        self.cor = cor
+        self.limit = limit
+
+    def __aiter__(self):
+        return self
+
+    @asyncio.coroutine
+    def __anext__(self):
+        val = yield from self.cor(self.limit)
+        if val == b'':
+            raise StopAsyncIteration
+        return val
+
 class job:
     def __init__(self, loop, save_stdout=False, save_stderr=False, save_mixed=False, echo=False, limit=(1024*4)):
         self.loop = loop
-        #self.stdout = bytearray()
-        #self.stderr = bytearray()
         self.save_stdout = save_stdout
         self.save_stderr = save_stderr
         self.save_mixed = save_mixed
@@ -63,7 +79,7 @@ class job:
                 self.buf_mixed.extend(data)
             if self.save_stderr:
                 self.buf_stderr.extend(data)
-        
+
     async def finish(self):
         if not self._started:
             await self.start()
@@ -77,6 +93,10 @@ class job:
     async def stdout(self):
         data = await self.process.stdout.read(self.limit)
         return data
+
+    def stdout_iter(self):
+        iter = output(self.process.stdout.read, limit=self.limit)
+        return iter
 
     async def stderr(self):
         data = await self.process.stderr.read(self.limit)
@@ -169,7 +189,18 @@ async def test_buf_saves(loop):
     j.command('./test2.sh', '5')
     await j.finish()
     print_bufs(j)
-    
+
+async def output_with_iter(j):
+    async for data in j.stdout_iter():
+        print(data.decode())
+
+async def test_async_iteration(loop):
+    j = job(loop)
+    j.command('./test2.sh', '5')
+    await j.start()
+    loop.create_task(output_with_iter(j))
+    await j.finish()
+
 async def main(loop):
     j = job(loop, save_stdout=True, save_stderr=True)
     j.command('./test2.sh', '5')
@@ -188,7 +219,8 @@ else:
     loop = asyncio.get_event_loop()
 
 #f = asyncio.Future(loop=loop)
-results = loop.run_until_complete(test_buf_saves(loop))
+#results = loop.run_until_complete(test_buf_saves(loop))
+loop.run_until_complete(test_async_iteration(loop))
 loop.close()
 
 # j.cmd = ['sleep', '1']
