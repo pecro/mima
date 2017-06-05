8fb66a65a5054f183619d244793558320f5e0d71
pecro <pecro@users.noreply.github.com>
2017-06-04 22:06:52 -0400

    added initial process monitoring

--- job.py
+++ job.py
@@ -6,7 +6,7 @@ Some documentation for this module
 
 import asyncio
 import sys
-
+import psutil
 
 # job.start()
 # job.add_start_callback()
@@ -40,9 +40,10 @@ class Refuture:
         self.finished.set_result(None)
 
 
-# A helper class to allow async iteration on process output.
-class _output:
+class _Output:
+    """A helper class to allow async iteration on process output."""
     def __init__(self, rcvd):
+        """rcvd is a Refuture"""
         self.rcvd = rcvd
 
     def __aiter__(self):
@@ -59,7 +60,7 @@ class _output:
 
 class job:
     def __init__(self, loop, save_stdout=False, save_stderr=False,
-                 save_mixed=False, echo=False, limit=(512)):
+                 save_mixed=False, echo=False, limit=(1024*4)):
         self.loop = loop
         self.save_stdout = save_stdout
         self.save_stderr = save_stderr
@@ -71,6 +72,7 @@ class job:
         self.started = asyncio.Future()
         self.stdout_rcvd = None
         self.stdout_updater_finished = asyncio.Future()
+        self.monitor = None
 
     def command(self, *args):
         self.command = args
@@ -93,6 +95,7 @@ class job:
             if self.save_stderr:
                 asyncio.ensure_future(self.stderr_saver())
         self.started.set_result(None)
+        self.monitor = asyncio.ensure_future(treestat(self.process.pid))
 
     async def stdout_saver(self):
         rcvd = self.stdout_rcvd
@@ -120,6 +123,7 @@ class job:
     async def finish(self):
         await self.started
         await self.process.wait()
+        self.monitor.cancel()
         await self.stdout_updater_finished
         print('Process end: {}'.format(self.process.pid))
 
@@ -143,7 +147,7 @@ class job:
         self.stdout_updater_finished.set_result(None)
 
     def stdout_iter(self):
-        iter = _output(self.stdout_rcvd)
+        iter = _Output(self.stdout_rcvd)
         return iter
 
     async def stdout(self):
@@ -152,30 +156,20 @@ class job:
         return rcvd.current.result()
 
 
-class chunk:
-    def __init__(self, j):
-        self.j = j
+def get_root_process(pid):
+    root = psutil.process(pid)
+    while root.parent():
+        print('pid={} has parent'.format(pid))
+        root = root.parent()
+    return root
 
-    def __aiter__(self):
-        return self
 
-    @asyncio.coroutine
-    def __anext__(self):
-        val = yield from self.j.process.stdout.read(1000)
-        if val == b'':
-            raise StopAsyncIteration
-        return val
-
-# @asyncio.coroutine
-# def chunk(j):
-#     yield from j.process.read()
-
-# async def printer(j):
-#     ch = chunk(j)
-#     async for data in ch:
-#         print('*** stdout ***')
-#         print(data.decode().rstrip())
-#     return 'fin'
+async def treestat(pid, interval=5):
+    """Periodically report process information."""
+    root = get_root_process(pid)
+    while True:
+        print('{} cpu={} mem={}'.format(root.name(), root.memory_info()['rss']))
+        await asyncio.sleep(interval)
 
 async def printer(io, name):
     count = 1
@@ -186,20 +180,6 @@ async def printer(io, name):
         print('*** {}({}) ***'.format(name, count))
         count += 1
         print(data.decode().rstrip())
-
-# async def printer(j,fd):
-#     if fd == 1:
-#          data = await j.stdout()
-#          print('*** stdout ***')
-#          print(data)
-#     if fd == 2:
-#         async for data in j.stderr():
-#             print('*** stderr ***')
-#             print(data)
-#     if fd == 3:
-#         async for data in j.output():
-#             print('*** either ***')
-#             print(data)
 
 
 def print_bufs(j):
@@ -234,7 +214,7 @@ async def output_with_iter(j):
 
 async def test_async_iteration(loop):
     j = job(loop, save_stdout=True)
-    j.command('./test2.sh', '5')
+    j.command('./test2.sh', '50')
     await j.start()
     asyncio.ensure_future(output_with_iter(j))
     await j.finish()
